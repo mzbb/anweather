bbf4d1b5dcd825e7e2c65080b345894e7ef3db91
boboo <2448745208@qq.com>
2016-05-21 21:27:31 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ src/main/java/com/anweather/app/model/Province.java
@@ -0,0 +1,34 @@
+package com.anweather.app.model;
+
+/**
+ * Created by mzbb on 2016/5/21.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
